efea8546f87cc7eff0c6c360025c2ae8328e62d7
j.kesraoui <j.kesraoui@pimenko.com>
2025-04-23 07:23:06 +0200

    Fixes a bug that prevented the course from being used after restoration from a course in another format (#161)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025032603;
+$plugin->version = 2025042303;
 $plugin->component = 'format_ludilearn';
 $plugin->release = '1.0.1';
 $plugin->requires = 2022041900;
