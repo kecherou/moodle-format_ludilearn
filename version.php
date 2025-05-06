9ac8312060603600bcb0b4e0789650422e03dc7e
j.kesraoui <j.kesraoui@pimenko.com>
2025-05-06 06:58:44 +0200

    Change version and release

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025042302;
+$plugin->version = 2025050620;
 $plugin->component = 'format_ludilearn';
-$plugin->release = '1.0.1-Moodle4.4';
+$plugin->release = '1.0.2-Moodle4.4';
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
