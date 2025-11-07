0add0702092b36ad88889bdc6b4a4a681c26d8f9
kecherou <j.kesraoui@pimenko.com>
2025-11-07 12:27:20 +0700

    Improve report interface and include manual assignment table in backup/restore process.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025110410;
+$plugin->version = 2025110700;
 $plugin->component = 'format_ludilearn';
 $plugin->release = '1.0.2';
 $plugin->requires = 2022041900;
