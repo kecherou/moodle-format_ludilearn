0ea3e428808cf23f301ee6abb99c555fe61184ea
j.kesraoui <j.kesraoui@pimenko.com>
2026-02-03 09:58:48 +0100

    Update version.php

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026011200;
+$plugin->version = 2026011230;
 $plugin->component = 'format_ludilearn';
-$plugin->release = '1.0.2';
+$plugin->release = '1.1';
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
